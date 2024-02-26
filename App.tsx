990230a7fe959bdbc3d02727c0aa056b694d04f5
lucaschier <ens-lucasschier@ugv.edu.br>
2024-02-26 14:05:41 -0300

    feat: :art: Criando o componente de container de navegação
    Aula 01 - Conhecendo o React-Navigation (Container de navegação)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { Poppins_400Regular, useFonts } from '@expo-google-fonts/poppins';
-import React from 'react';
-import { StatusBar } from 'react-native';
-import 'react-native-gesture-handler';
-import Home from './src/paginas/Home';
+import { Poppins_400Regular, useFonts } from "@expo-google-fonts/poppins";
+import React from "react";
+import { StatusBar } from "react-native";
+import "react-native-gesture-handler";
+import Navigation from "./src/routes/navigation";
+
 export default function App() {
   let [fontsLoaded, fontError] = useFonts({
     "PoppinsRegular": Poppins_400Regular,
@@ -14,7 +15,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Navigation />
       <StatusBar backgroundColor="#000" />
     </>
   );
