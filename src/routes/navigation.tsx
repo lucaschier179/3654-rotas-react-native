ce01d6d0c4246ff3c776b65d49597a3883c3f69e
lucaschier <ens-lucasschier@ugv.edu.br>
2024-02-26 17:55:58 -0300

    feat: :art: Aula 03 - Acessando abas (Tab navigation)

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -1,10 +1,23 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "../paginas/Home";
-import Login from "../paginas/Login";
 import Cadastro from "../paginas/Cadastro";
+import Home from "../paginas/Home";
+import ListaPets from "../paginas/ListaPets";
+import Login from "../paginas/Login";
+import Mensagem from "../paginas/Mensagem";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabRoutes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ListaPets" component={ListaPets} />
+      <Tab.Screen name="Mensagens" component={Mensagem} />
+    </Tab.Navigator>
+  )
+}
 
 export default function Navigation() {
   return (
