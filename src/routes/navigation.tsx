988df070b0534b0072bf61b3c595f40af10df607
lucaschier <ens-lucasschier@ugv.edu.br>
2024-02-26 14:34:19 -0300

    feat: :art: Entendendo o funcionamento da navegação com Stack
    Aula 01 - renderizando componentes (Stack Navigation)

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -1,9 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../paginas/Home";
+import Login from "../paginas/Login";
+
+const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
