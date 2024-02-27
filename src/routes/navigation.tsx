196e223ecd009dde7e2868572c7e54cfdbf8116b
lucaschier <ens-lucasschier@ugv.edu.br>
2024-02-27 08:56:48 -0300

    feat: :art: Aula 03 - Acessando abas (Customização de abas)

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Image } from "react-native";
 import Cadastro from "../paginas/Cadastro";
 import Home from "../paginas/Home";
 import ListaPets from "../paginas/ListaPets";
@@ -12,9 +13,27 @@ const Tab = createBottomTabNavigator();
 
 export function TabRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="ListaPets" component={ListaPets} />
-      <Tab.Screen name="Mensagens" component={Mensagem} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Tab.Screen
+        name="Lista de Pets"
+        component={ListaPets}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("../../assets/pets-green.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Mensagens"
+        component={Mensagem}
+      />
     </Tab.Navigator>
   )
 }
