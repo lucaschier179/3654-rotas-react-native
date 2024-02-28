c134b7bf341d6715b50e825c198e8de267df66c9
lucaschier <ens-lucasschier@ugv.edu.br>
2024-02-28 08:27:26 -0300

    fix: :bug: Renomeado os mocks em javascript para typescript

---
+++ src/mocks/conversas.ts
@@ -0,0 +1,14 @@
+export const conversas = [
+  {
+    nome: "Maria",
+    mensagem: "Olá, tudo bem?",
+  },
+  {
+    nome: "João",
+    mensagem: "Você quer saber mais sobre o Dunga?",
+  },
+  {
+    nome: "Pedro",
+    mensagem: "Me interessei muito por ele",
+  }
+]
